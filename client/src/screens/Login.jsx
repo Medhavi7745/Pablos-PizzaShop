d93fa31d1dbd45d873eefc753e58e67714f4fbd9
Medhavi7745 <153936177+Medhavi7745@users.noreply.github.com>
2024-07-08 00:46:28 +0530

    Add files via upload

---
+++ client/src/screens/Login.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import { Container, Form, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../actions/userAction";
+import { useSelector } from "react-redux";
+import Loader from "../components/Loader";
+import Success from "../components/Success";
+import Error from "../components/Error";
+const Login = () => {
+  const loginState = useSelector((state) => state.loginUserReducer);
+  const { error, success, loading } = loginState;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (localStorage.getItem("currentUser")) {
+      window.location.href = "/";
+    }
+  }, []);
+  const loginHandler = () => {
+    const user = { email, password };
+    dispatch(loginUser(user));
+  };
+  return (
+    <>
+      {loading && <Loader />}
+      {success && <Success success="User Logged In Successfully" />}
+      {error && <Error error="Something went wrong" />}
+      <Container>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter email"
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+            />
+          </Form.Group>
+          <Button variant="primary" onClick={loginHandler}>
+            Login
+          </Button>
+        </Form>
+      </Container>
+    </>
+  );
+};
+
+export default Login;
