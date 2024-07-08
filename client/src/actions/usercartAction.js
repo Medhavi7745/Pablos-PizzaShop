6405a95269fb275f599376ff78e836470d468b34
Medhavi7745 <153936177+Medhavi7745@users.noreply.github.com>
2024-07-08 12:56:43 +0530

    Update usercartAction.js

--- client/src/actions/usercartAction.js
+++ client/src/actions/usercartAction.js
@@ -1,16 +1,21 @@
 import axios from "axios";
 
+axios.defaults.withCredentials = true;
+
 export const placeOrder = (token, subTotal) => async (dispatch, getState) => {
   dispatch({ type: "PLACE_ORDER_REQUEST" });
   const currentUser = getState().loginUserReducer.currentUser;
   const cartItems = getState().cartReducer.cartItems;
   try {
-    await axios.post("/api/orders/placeorder", {
-      token,
-      subTotal,
-      currentUser,
-      cartItems,
-    });
+    await axios.post(
+      "https://pablos-pizza-shop.vercel.app/api/orders/placeorder",
+      {
+        token,
+        subTotal,
+        currentUser,
+        cartItems,
+      }
+    );
     dispatch({ type: "PLACE_ORDER_SUCCESS" });
     // console.log(res);
   } catch (error) {
@@ -25,9 +30,12 @@ export const getUserOrders = () => async (dispatch, getState) => {
     type: "USER_ORDER_REQUEST",
   });
   try {
-    const response = await axios.post("/api/orders/getuserorder", {
-      userid: currentUser._id,
-    });
+    const response = await axios.post(
+      "https://pablos-pizza-shop.vercel.app/api/orders/getuserorder",
+      {
+        userid: currentUser._id,
+      }
+    );
     // console.log(response);
     dispatch({ type: "USER_ORDER_SUCCESS", payload: response.data });
   } catch (error) {
